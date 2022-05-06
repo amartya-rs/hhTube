8dbe091d588af6ae5c6e6825319580094251aa6a
amartya-rs <saha.amartya@outlook.com>
2022-05-06 12:18:01 +0530

    feat: footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.css";
+import { MdFavoriteBorder } from "react-icons/md";
+
+const Footer = () => {
+   return (
+      <footer className="h6 font-medium mt-6">
+         Made with by <MdFavoriteBorder color="white" /> amartyars
+      </footer>
+   );
+};
+
+export { Footer };
