13457d2b27ab4c89538abd3e3e4d20cf5992380b
amartya-rs <saha.amartya@outlook.com>
2022-05-17 11:16:55 +0530

    refactor: api call status

--- src/utils/useAxios.js
+++ src/utils/useAxios.js
@@ -5,19 +5,21 @@ export const useAxios = () => {
    const [response, setResponse] = useState(undefined);
    const [error, setError] = useState("");
    const [loading, setLoading] = useState(true);
+   const [status, setStatus] = useState("");
 
    const apiCall = async (requiredData, axiosParams) => {
       try {
          const res = await axios.request(axiosParams);
+         setStatus(res.status);
          requiredData
             ? setResponse(res.data[requiredData])
             : setResponse(res.data);
       } catch (error) {
-         setError(error);
+         setError(error.response.data.errors);
       } finally {
          setLoading(false);
       }
    };
 
-   return { response, error, loading, apiCall, setResponse };
+   return { response, error, loading, status, apiCall, setResponse, setError };
 };
