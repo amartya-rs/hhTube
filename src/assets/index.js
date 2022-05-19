33debc5f92236e7e50989839ca6c6a85e0d5bca6
amartya-rs <saha.amartya@outlook.com>
2022-05-19 17:43:54 +0530

    added image

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,6 @@ import eminem from "./eminem.webp";
 import beatboxing from "./beatboxing.jpg";
 import bboying from "./bboying.jpg";
 import page_not_found from "./page_not_found.jpg";
+import user_profile from "./user_profile.png";
 
-export { banner, eminem, beatboxing, bboying, page_not_found };
+export { banner, eminem, beatboxing, bboying, page_not_found, user_profile };
